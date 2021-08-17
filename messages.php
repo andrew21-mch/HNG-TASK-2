e90e74aa115a8e7a21856593322e4676e8bcaa4f
andrew21-mch <nfonandrew73@gmail.com>
2021-08-17 23:20:08 +0100

    check this out

--- messages.php
+++ messages.php
@@ -21,30 +21,33 @@
         include_once "config.php";
 
         $sql = "SELECT * FROM messages";
-        $query = mysqli_query($conn, $sql);
+        $query = mysqli_query($conn, $sql);?>
 
-            echo "<table>" . "<tr>".
-                    "<th>Message Id</th>".
-                    "<th>Email of Sender</th>".
-                " <th>Name of Sender</th>".
-                    "<th>Subject</th>".
-                    "<th>Content</th>".
-                "</tr>";
+            <table border="1">
+                <tr>
+                    <th>Message Id</th>
+                    <th>Email of Sender</th>
+                    <th>Name of Sender</th>
+                    <th>Subject</th>
+                    <th>Content</th>
+                </tr>
 
+                <?php
                 while($row = mysqli_fetch_assoc($query)){
-                    echo "<tr>";
-                    echo "<td>".$row["Message_id"]."</td>" .
-                        "<td>". $row["Message_From_Email"]. "</td>".
-                        "<td>". $row["Message_From_Name"]."</td>".
-                        "<td>". $row["Message_Subject"]."</td>".
-                        "<td>". $row["Message_body"]. "</td>" ."<tr>";
+                    ?>
+                    <tr>
+                        <td> <?php echo $row["Message_id"]?> </td> 
+                        <td> <?php echo $row["Message_From_Email"]?> </td>
+                        <td> <?php echo $row["Message_From_Name"]?> </td>
+                        <td> <?php echo $row["Message_Subject"]?> </td>
+                        <td> <?php echo $row["Message_body"] ?></td>
                 }       
-                echo "</table>";
+                </table>
 
         
-        mysqli_close($conn);
+        <?php mysqli_close($conn);
+        }?>
     
     
-?>
 </body>
  </html>
