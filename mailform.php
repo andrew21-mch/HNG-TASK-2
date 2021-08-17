0fcd54b30a89a581ff0b9a2d2cb335db974ec142
andrew21-mch <nfonandrew73@gmail.com>
2021-08-17 21:43:40 +0100

    this is getting rough

--- mailform.php
+++ mailform.php
@@ -7,15 +7,15 @@ include "config.php";
         $subject = $_POST['subject'];
         $message = $_POST['message'];
 
-        $sql = "INSERT INTO `messages` (`Message_From_Email`, `Message_From_Name`, `Message_Subject`, `Message_body`) 
-                        VALUES ($email, $name, $subject, $message)";
-            if(mysqli_query($conn,$sql)){
-                echo"<script>alert('AMessage Delivered');</script>";
+        $sql = "INSERT INTO `messages` (`Message_From_Email`, `Message_From_Name`, `Message_Subject`, `Message_body`) VALUES ($email, $name, $subject, $message)";
+        $query = mysqli_query($conn,$sql);
+            if($query){
+                echo"<script>alert('Message Delivered')</script>";
             }
             else{
             echo "ERROR:". $sql. "<br>". mysqli_error($conn);
             }
     }
     else{
-    header("Location: ../register.html");
+    header("Location: index.php");
     }
