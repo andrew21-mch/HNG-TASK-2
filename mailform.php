7abf713fdd77dc6d7a91378d5a61cfa1a85cb067
andrew21-mch <nfonandrew73@gmail.com>
2021-08-17 21:38:23 +0100

    update

--- mailform.php
+++ mailform.php
@@ -1,18 +1,21 @@
 <?php
 include "config.php";
 
-if(isset($_POST['submit'])){
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+    if(isset($_POST['submit'])){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
 
-    $sql = "INSERT INTO `messages` (`Message_From_Email`, `Message_From_Name`, `Message_Subject`, `Message_body`) 
-                    VALUES ($email, $name, $subject, $message)";
-    if($sql){
-       echo " <script>alert('Message successfully sent')</script>";
+        $sql = "INSERT INTO `messages` (`Message_From_Email`, `Message_From_Name`, `Message_Subject`, `Message_body`) 
+                        VALUES ($email, $name, $subject, $message)";
+            if(mysqli_query($conn,$sql)){
+                echo"<script>alert('AMessage Delivered');</script>";
+            }
+            else{
+            echo "ERROR:". $sql. "<br>". mysqli_error($conn);
+            }
     }
     else{
-        echo " <script>alert('Trouble Sending Email')</script>";
+    header("Location: ../register.html");
     }
-}
