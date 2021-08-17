25d8b5dc539a07b3d8f6d29e5d3cb81f7681ad4c
andrew21-mch <nfonandrew73@gmail.com>
2021-08-17 14:13:59 +0100

    mail

--- emailcontact.php
+++ emailcontact.php
@@ -2,10 +2,11 @@
 
 if(isset($_POST['submit'])) {
 
+$email = $_POST['email']
 $to = "nfonandrew73@gmail.com";
 $subject = $_POST['subject'];
 $message = $_POST['message'];
-$headers = "From: webmaster@example.com" . "\r\n" .
+$headers = "From: $email" . "\r\n" .
 "CC: nfonandrew73@gmail.com";
 
 mail($to,$subject,$message,$headers);
