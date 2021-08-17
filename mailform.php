dff8be1fcf1e4ddeae4833438df8db353b1f9ce0
andrew21-mch <nfonandrew73@gmail.com>
2021-08-17 19:58:21 +0100

    database

---
+++ mailform.php
@@ -0,0 +1,15 @@
+<?php
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $sql = "INSERT INTO `Messages` (`Message_From_Email`, `Message_From_Name`, `Message_Subject`, `Message_body`) VALUES ($email, $name, $subject, $message)";
+    if($sql){
+       echo " <script>alert('Message successfully sent')</script>";
+    }
+    else{
+        echo " <script>alert('Trouble Sending Email')</script>";
+    }
+}
