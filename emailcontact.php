51dae43e8a0794318c7bac1c6b9183e03a7926df
andrew21-mch <nfonandrew73@gmail.com>
2021-08-17 14:28:56 +0100

    testing

--- emailcontact.php
+++ emailcontact.php
@@ -6,15 +6,12 @@
          $message .= "<h1>This is headline.</h1>";
          
          $header = "From:nfontatah73@gmail.com \r\n";
-         $header .= "Cc:nfontatah73@gmail.com \r\n";
-         $header .= "MIME-Version: 1.0\r\n";
-         $header .= "Content-type: text/html\r\n";
+    
          
-         $retval = mail ($to,$subject,$message,$header);
-         
-         if( $retval == true ) {
+         if(mail ($to,$subject,$message,$header)){
             echo "Message sent successfully...";
-         }else {
+         }
+         else {
             echo "Message could not be sent...";
          }
       ?>
