1029ccd96a945af47a92485ebe636f7493f0054f
andrew21-mch <nfonandrew73@gmail.com>
2021-08-17 22:48:01 +0100

    working on updates

--- messages.php
+++ messages.php
@@ -18,25 +18,24 @@ $sql = "SELECT * FROM messages";
 $query = mysqli_query($conn, $sql);
 
 $row = mysqli_fetch_assoc($query);
-    echo "<table>";
-    echo "<tr>
-            <th>Message Id</th>
-            <th>Email of Sender</th>
-            <th>Name of Sender</th>
-            <th>Subject</th>
-            <th>Content</th>
-        </tr>";
+    echo "<table style='border: 1'>" . "<tr>".
+            "<th>Message Id</th>".
+            "<th>Email of Sender</th>".
+           " <th>Name of Sender</th>".
+            "<th>Subject</th>".
+            "<th>Content</th>".
+        "</tr>";
 
-        while($row){
+        while($row>0){
             echo "<tr>";
             echo "<td>".$row["Message_id"]."</td>" .
                  "<td>". $row["Message_From_Email"]. "</td>".
                  "<td>". $row["Message_From_Name"]."</td>".
                  "<td>". $row["Message_Subject"]."</td>".
-                 "<td>". $row["Message_body"]. "</td>";
-           echo "<tr>";
-    echo "</table>";
-  mysqli_close($conn);
+                 "<td>". $row["Message_body"]. "</td>" ."<tr>" . 
+                 
+        "</table>";
+
  }
  ?>
  </html>
