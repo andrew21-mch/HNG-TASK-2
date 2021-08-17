e8b9ab33379a6699da9b5ca693ae2d18c192f70b
andrew21-mch <nfonandrew73@gmail.com>
2021-08-17 21:54:39 +0100

    string for all

--- mailform.php
+++ mailform.php
@@ -7,7 +7,7 @@
         $subject = $_POST['subject'];
         $message = $_POST['message'];
 
-        $sql = "INSERT INTO messages (Message_From_Email, Message_From_Name, Message_Subject, Message_body) VALUES ($email, $name, $subject, $message)";
+        $sql = "INSERT INTO messages (Message_From_Email, Message_From_Name, Message_Subject, Message_body) VALUES ('$email', '$name', '$subject', '$message')";
         $query = mysqli_query($conn,$sql);
             if($query){
                 echo"<script>alert('Message Delivered')</script>";
