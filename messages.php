cc07644e89dab60179bf05122e62fb3f769bbd41
andrew21-mch <nfonandrew73@gmail.com>
2021-08-17 22:38:05 +0100

    another update

--- messages.php
+++ messages.php
@@ -5,23 +5,24 @@ include_once "config.php";
 $sql = "SELECT * FROM messages";
 $query = mysqli_query($conn, $sql);
 
-$row = mysqli_fetch_array($sql);
-while($row){
-    echo "<table>".
-        "<tr>".
-            "<th>Message Id</th>".
-            "<th>Email of Sender</th>".
-            "<th>Name of Sender</th>".
-            "<th>Subject</th>
-            <th>Content</th>".
-        "</tr>
-        <tr>".
-           " <td>".$row['Message_id']."</td>".
-            "<td>".$row['Message_From_Email']."</td>".
-            "<td>".$row['Message_From_Name']."</td>".
-            "<td>".$row['Message_Subject']."</td>".
-            "<td>".$row['Message_body']."</td>".
+$row = mysqli_fetch_assoc($query);
+    echo "<table>";
+    echo "<tr>
+            <th>Message Id</th>
+            <th>Email of Sender</th>
+            <th>Name of Sender</th>
+            <th>Subject</th>
+            <th>Content</th>
+        </tr>";
 
-        "</tr>".
-    "</table>";
-}
+        while($row){
+            echo "<tr>";
+            echo "<td>".$row["Message_id"]."</td>" .
+                 "<td>". $row["Message_From_Email"]. "</td>".
+                 "<td>". $row["Message_From_Name"]."</td>".
+                 "<td>". $row["Message_Subject"]."</td>".
+                 "<td>". $row["Message_body"]. "</td>".
+        "<tr>";
+    echo "</table>";
+  mysqli_close($conn);
+ }
