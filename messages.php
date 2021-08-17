f7d14dd013fc5681147218881631b46284a10f9f
andrew21-mch <nfonandrew73@gmail.com>
2021-08-17 23:45:36 +0100

    i am tired

--- messages.php
+++ messages.php
@@ -21,6 +21,9 @@
             background-color: white;
             height: 100%
         }
+        tr{
+            height: 30px;
+        }
     </style>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
