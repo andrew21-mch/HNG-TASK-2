e21f1a78d5b492f7a2b448ae0828e3daab7e21b4
andrew21-mch <nfonandrew73@gmail.com>
2021-08-17 23:00:30 +0100

    ok na

--- messages.php
+++ messages.php
@@ -7,42 +7,44 @@
             background-color: gray;
         }
         tr{
-            border-style: groove;
+            border-style: solid;
         }
         td{
             border-style:solid;
         }
     </style>
     </head>
+    <body>
+        <?php
+
+        use mysql_xdevapi\Table;
+        include_once "config.php";
+
+        $sql = "SELECT * FROM messages";
+        $query = mysqli_query($conn, $sql);
+
+            echo "<table style='border: 1'>" . "<tr>".
+                    "<th>Message Id</th>".
+                    "<th>Email of Sender</th>".
+                " <th>Name of Sender</th>".
+                    "<th>Subject</th>".
+                    "<th>Content</th>".
+                "</tr>";
+
+                while($row = mysqli_fetch_assoc($query)){
+                    echo "<tr>";
+                    echo "<td>".$row["Message_id"]."</td>" .
+                        "<td>". $row["Message_From_Email"]. "</td>".
+                        "<td>". $row["Message_From_Name"]."</td>".
+                        "<td>". $row["Message_Subject"]."</td>".
+                        "<td>". $row["Message_body"]. "</td>" ."<tr>";
+                }       
+                echo "</table>";
+
+        
+        mysqli_close($conn);
     
-<?php
-
-
-use mysql_xdevapi\Table;
-include_once "config.php";
-
-$sql = "SELECT * FROM messages";
-$query = mysqli_query($conn, $sql);
-
-    echo "<table style='border: 1'>" . "<tr>".
-            "<th>Message Id</th>".
-            "<th>Email of Sender</th>".
-           " <th>Name of Sender</th>".
-            "<th>Subject</th>".
-            "<th>Content</th>".
-        "</tr>";
-
-        while($row = mysqli_fetch_assoc($query)){
-            echo "<tr>";
-            echo "<td>".$row["Message_id"]."</td>" .
-                 "<td>". $row["Message_From_Email"]. "</td>".
-                 "<td>". $row["Message_From_Name"]."</td>".
-                 "<td>". $row["Message_Subject"]."</td>".
-                 "<td>". $row["Message_body"]. "</td>" ."<tr>";
-          }       
-        echo "</table>";
-
- 
- mysqli_close($conn);
- ?>
+    
+?>
+</body>
  </html>
